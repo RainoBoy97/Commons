f6c00bfcd3b76d8432a7dde0a971b8f57994605d
Raino <RainoBoy97@gmail.com>
2014-07-09 19:28:50 +0200

    Add FireworkUtility, currently only one method which allows playing a fireworkeffect instantly

---
+++ src/main/java/me/raino/commons/utility/FireworkUtility.java
@@ -0,0 +1,24 @@
+package me.raino.commons.utility;
+
+import org.bukkit.FireworkEffect;
+import org.bukkit.Location;
+import org.bukkit.entity.Firework;
+import org.bukkit.inventory.meta.FireworkMeta;
+
+public class FireworkUtility {
+
+    public static void playInstant(Location location, FireworkEffect effect) {
+        Firework firework = (Firework) location.getWorld().spawn(location, Firework.class);
+        FireworkMeta fireworkMeta = firework.getFireworkMeta();
+        fireworkMeta.clearEffects();
+        fireworkMeta.setPower(1);
+        fireworkMeta.addEffect(effect);
+        firework.setFireworkMeta(fireworkMeta);
+        firework.detonate();
+        if (!firework.isDead())
+            firework.remove();
+    }
+
+    private FireworkUtility() {}
+
+}

a8d2df10b57ba6b7c3a7a7999d43006c1d13fc03
Raino <RainoBoy97@gmail.com>
2014-07-11 14:23:04 +0200

    Changed variable name

--- src/main/java/me/raino/commons/utility/PlayerUtility.java
+++ src/main/java/me/raino/commons/utility/PlayerUtility.java
@@ -1,18 +1,18 @@
 package me.raino.commons.utility;
 
 import me.raino.commons.messaging.localization.Language;
-import me.raino.commons.reflection.ReflectionClass;
 import me.raino.commons.reflection.SafeField;
+import me.raino.commons.reflection.SafeMethod;
 
 import org.bukkit.entity.Player;
 
 public class PlayerUtility {
 
     public static Language getLanguage(Player player) {
-        ReflectionClass<?> clazz = ReflectionClass.of(player.getClass());
-        Object craftPlayer = clazz.getMethod("getHandle").invoke(player);
-        SafeField<String> f = ReflectionClass.of(craftPlayer.getClass()).getField("locale");
-        return Language.fromLocale(f.get(craftPlayer));
+        SafeMethod<Object> handle = SafeMethod.of(player.getClass(), "getHandle");
+        Object craftPlayer = handle.invoke(player);
+        SafeField<String> locale = SafeField.of(craftPlayer.getClass(), "locale");
+        return Language.fromLocale(locale.get(craftPlayer));
     }
 
     private PlayerUtility() {}
